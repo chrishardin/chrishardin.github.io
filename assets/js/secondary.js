f260daebcc666e0a4d363501171e9f29867fea36
Chris Hardin <christopher.hardin7@gmail.com>
2022-05-19 00:19:45 -0400

    WIP

--- assets/js/secondary.js
+++ assets/js/secondary.js
@@ -30,12 +30,16 @@
   var showValue = document.getElementById("display-value");
   inputStocksSubmit.addEventListener("click", runInputStocksSubmit);
   
-  document.getElementById('inputfile').addEventListener('change', function () {
-    var fr = new FileReader();
-    fr.onload = function () {
-      document.getElementById('output')
-        .textContent = fr.result;
-    }
 
-    fr.readAsText(this.files[0]);
-  }) 
+
+
+//   document.getElementById('inputfile').addEventListener('change', function () {
+//     var fr = new FileReader();
+//     fr.onload = function () {
+//       document.getElementById('output')
+//         .textContent = fr.result;
+//     }
+
+//     fr.readAsText(this.files[0]);
+//   }) 
+
---
+++ assets/js/zax.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+
+function getDateString() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const day =`${date.getDate()}`.padStart(2, '0');
+  return `${year}-${month}-${day}`
+}
+
+/*
+* Input files
+* -----------
+* - stocksInput.txt
+* - testStocksInput.txt
+*/
+
+const INPUTFILE = 'stocksInput.txt';
+const OUTPUTFILE = 'stocksOutput_' + getDateString() + '.csv';
+
+// If INPUTFILE is a .txt, only Symbol should be true
+const HEADER_CONFIG = [
+    {type : 'Symbol', bool : true},
+    {type : 'Name', bool : true},
+    {type : 'Industry Major', bool : true},
+    {type : 'Industry Minor', bool : true},
+    {type : 'Rank', bool : true},
+    {type : 'Value', bool : true},
+    {type : 'Growth', bool : true},
+    {type : 'Momentum', bool : true},
+    {type : 'VGM', bool : true},
+]
+
+// either all true, or all false
+const HEADER_CONFIG2 = [
+    {type : 'Net Income 4 Years Ago', bool : true},
+    {type : 'Net Income 3 Years Ago', bool : true},
+    {type : 'Net Income 2 Years Ago', bool : true},
+    {type : 'Net Income 1 Years Ago', bool : true},
+    {type : 'Net Income Most Recent', bool : true},
+    {type : 'Consecutive Net Income Growth', bool : true},
+    {type : 'Average Growth', bool : true},
+]
+
+let HEADERS = '';
+let HEADERS2 = '';
+
+HEADER_CONFIG.forEach(element => {
+    if(element.bool)
+        HEADERS += `${element.type},`
+})
+HEADER_CONFIG2.forEach(element => {
+    if(element.bool)
+        HEADERS2 += `${element.type},`
+})
+
+let header;
+if(HEADERS2)
+{
+    header = `${HEADERS}`;
+}
+else
+{
+    header = `${HEADERS.slice(0, HEADERS.length -1)}`;
+}
+
+let header2 = `${HEADERS2.slice(0, HEADERS2.length -1)}\n`;
+
+// console.log(header)
+// console.log(header2)
+functions.runProgram(INPUTFILE, OUTPUTFILE, header, header2);
